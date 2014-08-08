13da8bd4ad6574b0302aa71deb76183ede522b9b
dilir <dilir.khan@gmail.com>
2014-08-09 02:07:09 +0600

    Update graphics.r

--- graphics.r
+++ graphics.r
@@ -1,1 +1,13 @@
 with(iris, boxplot(Sepal.Width~Species,col=c("red","blue","yellow"),notch=T))
+
+# histogram with rug
+hist(iris$Sepal.Width, col="green")
+rug(iris$Sepal.Width,ticksize=0.01,line=2)
+#---------
+
+#boxplot with mean as abline (..benchmark..)
+boxplot(iris$Sepal.Width, col="red",notch=T)
+abline(h=mean(iris$Sepal.Width),lty=3, col=3, lwd=3)
+abline(a=2,b=.6)
+
+#---------
