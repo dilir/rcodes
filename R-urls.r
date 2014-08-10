26a653e129a3fd47632b66be23dc55794c67217e
dilir <dilir.khan@gmail.com>
2014-08-10 21:18:26 +0600

    Update R-urls.r

--- R-urls.r
+++ R-urls.r
@@ -1,3 +1,4 @@
 http://rstudio-pubs-static.s3.amazonaws.com/7953_4e3efd5b9415444ca065b1167862c349.html
 #Github basics
 #http://gitready.com/beginner/2009/01/21/pushing-and-pulling.html
+http://www.r-tutor.com/r-introduction/data-frame/data-import
