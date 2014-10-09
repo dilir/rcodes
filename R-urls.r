c25fbb1410b576ed642af9f740d554588e56e1a1
dilir <dilir.khan@gmail.com>
2014-10-09 16:38:01 +0600

    Update R-urls.r

--- R-urls.r
+++ R-urls.r
@@ -2,3 +2,4 @@ http://rstudio-pubs-static.s3.amazonaws.com/7953_4e3efd5b9415444ca065b1167862c34
 #Github basics
 #http://gitready.com/beginner/2009/01/21/pushing-and-pulling.html
 http://www.r-tutor.com/r-introduction/data-frame/data-import
+https://www.youtube.com/watch?v=pi1tm5RbSRg # hypothesis test and p-value
